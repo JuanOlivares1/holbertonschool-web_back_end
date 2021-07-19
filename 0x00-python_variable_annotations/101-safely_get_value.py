d46c84d0fbef4fe2d6923a86063eb91383e724c1
JuanOlivares1 <jdop2000@gmail.com>
2021-07-19 17:03:26 -0500

    typevar annotation

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 """ Module """
 from typing import Union, Any, TypeVar, Mapping
+dd = TypeVar('T')
 
 
-ddefault = TypeVar('T')
-
-def safely_get_value(dct: Mapping, key: Any, default: Union[ddefault, None] = None):
+def safely_get_value(dct: Mapping, key: Any, default: Union[dd, None] = None):
     """ Returns first item of list """
     if key in dct:
         return dct[key]
