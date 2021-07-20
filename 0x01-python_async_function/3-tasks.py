2dd532fe8a7a7856b69a76af8fdb7b94f243a99f
JuanOlivares1 <jdop2000@gmail.com>
2021-07-20 16:44:27 -0500

    creating tasks

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Module """
+import asyncio
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """ returns a asyncio.Task of a coro """
+    return asyncio.create_task(wait_random(max_delay))
