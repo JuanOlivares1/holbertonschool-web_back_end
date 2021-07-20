57c097c542c3687e2541ac176386daf7029d2fdf
JuanOlivares1 <jdop2000@gmail.com>
2021-07-20 16:21:21 -0500

    messuring time

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -11,4 +11,4 @@ def measure_time(n: int, max_delay: int) -> float:
     start = perf_counter()
     wait_n(n, max_delay)
     end = perf_counter()
-    return (end - start)
+    return (end - start) / n
