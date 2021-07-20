01f6bee12a99d49f566e998d488d39a839cfb397
JuanOlivares1 <jdop2000@gmail.com>
2021-07-20 15:59:35 -0500

    sorting list

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -9,8 +9,17 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """ Use previus func n times """
     delays = []
+    s_delays = []
 
     for x in range(n):
         delays.append(await wait_random(max_delay))
 
-    return delays
+    while delays:
+        minimum = delays[0]
+        for x in delays:
+            if x < minimum:
+                minimum = x
+        s_delays.append(minimum)
+        delays.remove(minimum)
+
+    return s_delays
