96ac93391ee76407dc9940833b126b2dbc574470
JuanOlivares1 <jdop2000@gmail.com>
2021-07-20 15:24:30 -0500

    async sleep(rand)

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """ Module """
-import asyncio, random
+import asyncio
+import random
 
 
-async def wait_random(max_delay = 10):
+async def wait_random(max_delay=10):
     rand = random.uniform(0, max_delay)
     await asyncio.sleep(rand)
     return rand
