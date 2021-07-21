13d80b53eb1ab5ad90010e7aca82651cb6bbd8eb
JuanOlivares1 <jdop2000@gmail.com>
2021-07-21 16:16:45 -0500

    pep8

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,9 +3,11 @@
 import asyncio
 import random
 
+
 async def async_generator():
+    """ async function """
     rand = 0
-    
+
     for x in range(10):
         rand = random.uniform(0, 10)
         await asyncio.sleep(1)
