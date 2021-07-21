d8a7edd3eacbc1f8330d908b18aaca56193c0547
JuanOlivares1 <jdop2000@gmail.com>
2021-07-21 16:59:45 -0500

    annotations

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """ Module """
 import asyncio
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
-    """ async for """    
-    return result = [i async for i in async_generator()]
-
+async def async_comprehension() -> List[float]:
+    """ asynchronous comprehension """
+    return [i async for i in async_generator()]
