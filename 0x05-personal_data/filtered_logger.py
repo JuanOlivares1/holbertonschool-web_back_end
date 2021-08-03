87fea2ced480a0b85ba54375fd0e333430e1cb55
JuanOlivares1 <jdop2000@gmail.com>
2021-08-03 09:11:08 -0500

    regex

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -7,7 +7,9 @@ from typing import List
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """ Filter data with regex and obfuscate """
+    msg = message
     for field in fields:
-        message = re.sub(r"(?<={}=)[^{}]*(?={})".format(field, separator, separator),
-                         redaction, message)
-    return message
+        msg = re.sub(r"(?<={}=)[^{}]*(?={})".format(field, separator,
+                                                    separator),
+                     redaction, msg)
+    return msg
