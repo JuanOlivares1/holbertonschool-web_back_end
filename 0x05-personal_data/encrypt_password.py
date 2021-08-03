cbaa2fe84dc0f896c8010bd17e70206a61e43231
JuanOlivares1 <jdop2000@gmail.com>
2021-08-03 18:22:05 -0500

    validate encryption

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -4,8 +4,15 @@ import bcrypt
 
 
 def hash_password(password: str) -> bytes:
-    """ encrypts password
+    """ Encrypts password
     """
     psswrd = bytes(password, 'utf-8')
     hashed = bcrypt.hashpw(psswrd, bcrypt.gensalt())
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ Validate that the provided password matches the hashed password
+    """
+    psswrd = bytes(password, 'utf-8')
+    return bcrypt.checkpw(psswrd, hashed_password)
