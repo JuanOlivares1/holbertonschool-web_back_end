82d1e97031f2ca25325424e344e4770afd07ae5c
JuanOlivares1 <jdop2000@gmail.com>
2021-08-03 20:32:08 -0500

    db con

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -54,11 +54,12 @@ def get_logger() -> logging.Logger:
 
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
-    """Connects to secure database"""
+    """ data base connection with env variables """
     user = os.getenv('PERSONAL_DATA_DB_USERNAME')
     password = os.getenv('PERSONAL_DATA_DB_PASSWORD')
     host = os.getenv('PERSONAL_DATA_DB_HOST')
     db = os.getenv('PERSONAL_DATA_DB_NAME')
 
-    return = mysql.connector.connect(user=user, password=password,
-                                     host=host, database=db)
+    cnx = mysql.connector.connect(user=user, password=password,
+                                  host=host, database=db)
+    return cnx
