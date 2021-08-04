f0f511a500e0664136f7e793e9665dc466f2e9b0
JuanOlivares1 <jdop2000@gmail.com>
2021-08-03 20:23:10 -0500

    db connection

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -54,12 +54,14 @@ def get_logger() -> logging.Logger:
 
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
-    """ Get data from db
+    """ Get data from data base
     """
     user = os.getenv("PERSONAL_DATA_DB_USERNAME"),
     password = os.getenv("PERSONAL_DATA_DB_PASSWORD"),
     host = os.getenv("PERSONAL_DATA_DB_HOST"),
     database = os.getenv("PERSONAL_DATA_DB_NAME")
+
     cnx = mysql.connector.connect(user=user, password=password,
                                   host=host, database=database)
+                                  
     return cnx
