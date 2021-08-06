cd30fe538803fb357c66bec0e63616e944e9f4bd
JuanOlivares1 <jdop2000@gmail.com>
2021-08-06 14:02:33 -0500

    base64 decode

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -28,7 +28,8 @@ class BasicAuth(Auth):
 
         return rtn[1]
 
-    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+    def decode_base64_authorization_header(self, base64_authorization_header:
+                                           str) -> str:
         """ Base64 decode
         """
         if base64_authorization_header is None:
