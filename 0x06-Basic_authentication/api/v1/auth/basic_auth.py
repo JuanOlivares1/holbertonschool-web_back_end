e5100b6c691eb4097010cb3f5831f4d151f9a864
JuanOlivares1 <jdop2000@gmail.com>
2021-08-06 13:39:48 -0500

    asic auth base64

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,4 +7,21 @@ from .auth import Auth
 class BasicAuth(Auth):
     """ Basic Auth class
     """
-    pass
+    def extract_base64_authorization_header(self, 
+                                            authorization_header: str) -> str:
+        """ Base64 extract
+        """
+        if authorization_header is None:
+            return None
+
+        if authorization_header is not str:
+            return None
+
+        if authorization_header[:5] != 'Basic':
+            return None
+        
+        rtn = authorization_header.split()
+        if len(rtn) < 1:
+            return None
+        
+        return rtn[1]
