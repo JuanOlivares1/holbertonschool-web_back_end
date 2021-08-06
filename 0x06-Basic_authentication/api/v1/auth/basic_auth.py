2c72a3a3a02fbf79ffb3ef3f25609820aded4c6e
JuanOlivares1 <jdop2000@gmail.com>
2021-08-06 12:54:50 -0500

    basic auth

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,18 +2,9 @@
 """ Module of API Basic Authentication
 """
 from .auth import Auth
-from models.user import User
 
 
 class BasicAuth(Auth):
     """ Basic Auth class
     """
-    def current_user(self, request=None) -> TypeVar('User'):
-        """Current user and basic authentication"""
-        a_header = self.authorization_header(request)
-        m_base64 = self.extract_base64_authorization_header(a_header)
-        n_base64 = self.decode_base64_authorization_header(m_base64)
-        user, password = self.extract_user_credentials(n_base64)
-        obj = self.user_object_from_credentials(user, password)
-
-        return obj
+    pass
