51f8975fe03d23250b704442d0d06b521c613739
JuanOlivares1 <jdop2000@gmail.com>
2021-08-11 18:45:40 -0500

    login route

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -59,14 +59,12 @@ def before_request():
             '/api/v1/forbidden/',
             '/api/v1/auth_session/login/'
         ]
-
-        if auth.authorization_header(request) is None and
-            auth.session_cookie(request) is None:
-            abort(401)
-            return None
-
-
         if auth.require_auth(request.path, paths):
+            if auth.authorization_header(request) is None and\
+              auth.session_cookie(request) is None:
+                abort(401)
+                return None
+
             if auth.authorization_header(request) is None:
                 abort(401)
                 return None
