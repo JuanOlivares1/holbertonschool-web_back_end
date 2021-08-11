59546a6601efac5e36dc3536d71cecea64418088
JuanOlivares1 <jdop2000@gmail.com>
2021-08-11 18:42:34 -0500

    login rout

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -56,8 +56,16 @@ def before_request():
         paths = [
             '/api/v1/status/',
             '/api/v1/unauthorized/',
-            '/api/v1/forbidden/'
+            '/api/v1/forbidden/',
+            '/api/v1/auth_session/login/'
         ]
+
+        if auth.authorization_header(request) is None and
+            auth.session_cookie(request) is None:
+            abort(401)
+            return None
+
+
         if auth.require_auth(request.path, paths):
             if auth.authorization_header(request) is None:
                 abort(401)
