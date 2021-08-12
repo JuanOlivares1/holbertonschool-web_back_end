a1ff01ab0ed8bad38a97915dd7a5b951123b937b
JuanOlivares1 <jdop2000@gmail.com>
2021-08-12 15:57:24 -0500

    logout

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -50,5 +50,5 @@ class SessionAuth(Auth):
         if user_id is None:
             return False
 
-        del self.user_id_by_session_id[session_id]
+        del SessionAuth.user_id_by_session_id[session_id]
         return True
