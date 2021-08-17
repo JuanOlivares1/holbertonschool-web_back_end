4655a23c61fecb7ae799804d27e2fe9273343f33
JuanOlivares1 <jdop2000@gmail.com>
2021-08-17 17:13:36 -0500

    db

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -33,9 +33,7 @@ class DB:
     def add_user(self, email: str, hashed_password: str) -> User:
         """ Method - create user
         """
-        if email is not None and hashed_password is not None:
-            newUser = User(email=email, hashed_password=hashed_password)
-            self._session.add(newUser)
-            self._session.commit()
-            return(newUser)
-        return None
+        newUser = User(email=email, hashed_password=hashed_password)
+        self._session.add(newUser)
+        self._session.commit()
+        return(newUser)
