a9fdf5bafe62ea100693d87ae95f5f2b2a501430
JuanOlivares1 <jdop2000@gmail.com>
2021-08-18 15:52:47 -0500

    db

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -10,7 +10,6 @@ from sqlalchemy.orm.exc import NoResultFound
 from user import Base, User
 
 
-
 class DB:
     """DB class
     """
@@ -24,7 +23,7 @@ class DB:
         self.__session = None
 
     @property
-    def _session(self) -> Session:
+    def _session(self):
         """Memoized session object
         """
         if self.__session is None:
