c32b6a05dac3b3750f3aaefe4456a88914c8c613
JuanOlivares1 <jdop2000@gmail.com>
2021-08-18 19:13:51 -0500

    auth loging

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -25,6 +25,16 @@ class Auth:
             user = self._db.add_user(email, h_passwd)
             return user
 
+    def valid_login(self, email: str, password: str) -> bool:
+        """ determines if login is correct
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            h_passwd = password.encode('utf-8')
+            return bcrypt.checkpw(h_passwd, user.hashed_password)
+        except NoResultFound:
+            return False
+
 
 def _hash_password(password: str) -> bytes:
     """ hashes a password with bcrypt
