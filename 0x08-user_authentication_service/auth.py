a43f5d13efbbb9520730f3a719111a7523c1f08f
JuanOlivares1 <jdop2000@gmail.com>
2021-08-18 19:25:41 -0500

    generating sessions

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -6,6 +6,7 @@ import uuid
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
+from typing import Union
 
 
 class Auth:
@@ -36,7 +37,7 @@ class Auth:
         except NoResultFound:
             return False
 
-    def create_session(self, email: str) -> str:
+    def create_session(self, email: str) -> Union[str, None]:
         """ generates sessions
         """
         try:
