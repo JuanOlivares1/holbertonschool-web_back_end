94808aa8d043241fca04ec47bdf1b17bcb13c8dd
JuanOlivares1 <jdop2000@gmail.com>
2021-08-19 13:25:32 -0500

    correct updat

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -44,7 +44,7 @@ class Auth:
             user = self._db.find_user_by(email=email)
             sess_id = _generate_uuid()
             self._db.update_user(user.id, session_id=sess_id)
-            return sess_id
+            return user.session_id
         except NoResultFound:
             return None
 
