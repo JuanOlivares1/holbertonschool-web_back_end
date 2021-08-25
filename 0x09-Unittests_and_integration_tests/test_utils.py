dafcb2a2d32a28a9be0d3ccbad6177df9809aca0
JuanOlivares1 <jdop2000@gmail.com>
2021-08-24 19:07:45 -0500

    pep8

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -8,11 +8,9 @@ class TestAccessNestedMap(Unittest.TestCase):
     """Test for utils
     """
     @parameterized.expand([
-       ({"a": 1}, ("a",), 1),
-       ({"a": {"b": 2}}, ("a",), {'b': 2}),
-       ({"a": {"b": 2}}, ("a", "b"), 2),
-    ])
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {'b': 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)])
     def test_access_nested_map(self, a: Dict, b: Tuple, out: Any):
-        """test cases for access_nested_map method
-        """
+        """test cases for access_nested_map method"""
         self.assertEqual(access_nested_map(a, b), out)
