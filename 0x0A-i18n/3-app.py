1d70ba46ab92a66dfa2709cc767180f78143463f
JuanOlivares1 <jdop2000@gmail.com>
2021-08-26 16:56:15 -0500

    translation en - fr

--- 0x0A-i18n/3-app.py
+++ 0x0A-i18n/3-app.py
@@ -2,7 +2,7 @@
 """Seting up basic Flask app
 """
 from flask import Flask, render_template, request
-from flask_babel import Babel
+from flask_babel import Babel, _
 
 app = Flask(__name__)
 
@@ -30,7 +30,7 @@ def get_locale():
 def index():
     """Route to root page
     """
-    return render_template('2-index.html')
+    return render_template('3-index.html')
 
 
 if __name__ == "__main__":
