dee742e86835ff5cd87751aa927d2792a31879b3
JuanOlivares1 <jdop2000@gmail.com>
2021-09-21 17:31:09 -0500

    mapping an array

--- 0x11-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x11-ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,5 +2,5 @@ export default function getListStudentIds(getListStudents) {
   if (!Array.isArray(getListStudents)) {
     return [];
   }
-  return getListStudents.map( function (x) { return x.id; });
+  return getListStudents.map((obj) => obj.id);
 }
